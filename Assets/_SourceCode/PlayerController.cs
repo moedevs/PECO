217ea1b4cba4120b0f69bd3c3e38cb612f0d9cf6
Volks <waterlockgames@gmail.com>
2019-03-18 20:52:43 +0000

    Auto stash before merge of "master" and "origin/master"
    Checking in Possession of pawns, bit janky but it "works". Camera is set to pawn once possessed. 
    
    Removed pawns ability to jump as it just let them float into space

--- Assets/_SourceCode/PlayerController.cs
+++ Assets/_SourceCode/PlayerController.cs
@@ -2,30 +2,35 @@
 
 public class PlayerController : MonoBehaviour
 {
-
+    //playercontroller functionality 
     private GameObject controlledPawn;
     private CharacterController pawnController;
     public Camera pawnCamera;
     private RaycastHit hitObj;
 
-
+    //input variables 
     public float speed = 12.0f;
-    public float turnSpeed = 5f;
-    public float jumpSpeed = 15.0f;
-    public float gravity = 20.0f;
+    public float turnSpeed = 2f;
     Vector3 moveDirection;
     Vector3 moveRotation;
 
-    // Start is called before the first frame update
+    //sets the position of the camera behind each pawn once possessed
+    private Vector3 CameraPosition = new Vector3(0.8f, 2.5f, -4.2f);
+
+
     void Start()
     {
         controlledPawn = GameObject.FindGameObjectsWithTag("PlayerControllable")[0];
         pawnController = controlledPawn.GetComponent<CharacterController>();
+
+        //Sets the camera to be a child of  the controlled pawn
         pawnCamera.transform.SetParent(controlledPawn.transform);
 
+        //force set the cameras position 
+        pawnCamera.transform.rotation = Quaternion.Euler(15f, 0f, 0f);
+        pawnCamera.transform.localPosition = CameraPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (controlledPawn != null)
@@ -35,37 +40,44 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void Movement()
+    {
+        moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+        moveRotation = new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f);
+
+        //converts move direction to world space
+        moveDirection = controlledPawn.transform.TransformVector(moveDirection);
+        moveDirection = moveDirection * speed;
+        
+        pawnController.Move(moveDirection * Time.deltaTime);
+        pawnController.transform.Rotate((moveRotation * turnSpeed), Space.Self);
+    }
+
     private void RayTrace()
     {
+        Debug.DrawRay(controlledPawn.transform.position, controlledPawn.transform.forward * 10f, Color.red);
 
-        if (Input.GetMouseButton(0))
+        //on LMB 
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Physics.Raycast(controlledPawn.transform.position, controlledPawn.transform.forward, out hitObj, 50, 1))
+            //if hit object is tagged as controllable, possess it 
+            if (Physics.Raycast(controlledPawn.transform.position, controlledPawn.transform.forward, out hitObj, 100, 1))
             {
-                Debug.DrawRay(controlledPawn.transform.position, controlledPawn.transform.forward * 50f, Color.red);
-                print(hitObj.collider.gameObject.name);
+                if (hitObj.transform.gameObject.tag == "PlayerControllable")
+                {
+                    ChangeControlledPawn(hitObj.transform.gameObject);
+                }
             }
         }
     }
 
-    private void Movement()
+    //sets controlled pawn to new pawn, resets camera on new pawn
+    private void ChangeControlledPawn(GameObject newPawn)
     {
-        moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
-        moveDirection = controlledPawn.transform.TransformVector(moveDirection);
-        moveDirection = moveDirection * speed;
-
-        moveRotation = new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f);
-
-        if (Input.GetButton("Jump"))
-        {
-            moveDirection.y = jumpSpeed;
-        }
-
-        // Apply gravity
-        moveDirection.y = moveDirection.y - ((gravity*6) * Time.deltaTime);
-
-        // Move the controller
-        pawnController.Move(moveDirection * Time.deltaTime);
-        pawnController.transform.Rotate((moveRotation * turnSpeed), Space.Self);
+        controlledPawn = newPawn;
+        pawnController = controlledPawn.GetComponent<CharacterController>();
+        pawnCamera.transform.SetParent(controlledPawn.transform);
+        pawnCamera.transform.localPosition = CameraPosition;
+        pawnCamera.transform.rotation = controlledPawn.transform.rotation * Quaternion.Euler(15f, 0f, 0f);
     }
 }
