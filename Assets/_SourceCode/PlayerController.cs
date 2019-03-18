2235dbd4d7a491481787527ecb8fdd8d71593da7
Volks <waterlockgames@gmail.com>
2019-03-18 08:06:16 +0000

    First start at setting up a player controller in Unity. Needs work, player can move forward (barely) and script possesses the first pawn it finds in world.

---
+++ Assets/_SourceCode/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private GameObject controlledPawn;
+    private CharacterController pawnController;
+    public Camera pawnCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlledPawn = GameObject.FindGameObjectsWithTag("PlayerControllable")[0];
+        pawnController = controlledPawn.GetComponent<CharacterController>();
+        pawnCamera.transform.SetParent(controlledPawn.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controlledPawn != null)
+        {
+            MoveForward();
+        }
+    }
+
+    private void MoveForward()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            pawnController.Move(new Vector3(0, 0, 1));
+        }
+    }
+}
