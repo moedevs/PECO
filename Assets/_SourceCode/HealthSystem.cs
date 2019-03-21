d93391635194da41a942bc4b1705ff6f80864682
jvyden420 <jvyden420@gmail.com>
2019-03-20 20:48:24 -0400

    Add basic HP system, added testing cubes

---
+++ Assets/_SourceCode/HealthSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    static int MaxHealth = 6;
+    public static int Health = 0;
+    public static bool fuckingDead = false;
+    void Start() {
+        Health = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    // spaghetti
+    public static void TakeDamage(int amount = 1) {
+        Health = Health - amount;
+        checkHP();
+    }
+    // code
+    public static void HealDamage(int amount = 1) {
+        Health = Health + amount;
+        checkHP();
+    }
+    // af
+    public static void checkHP() {
+        if (Health > MaxHealth) {
+            Health = MaxHealth;
+            return;
+        }
+        if (Health <= 0) {
+            fuckingDead = true;
+        }
+        Debug.Log(Health);
+    }
+}
